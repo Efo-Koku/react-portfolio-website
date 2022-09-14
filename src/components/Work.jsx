c962f42ab4de6bdecbc58149c705bc511774b19a
Efo-Koku Avornyo <EfoKoku@Efo-Kokus-MacBook-Pro.local>
2022-09-14 21:15:19 +0100

    updated links to demo and GitHub code

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -141,16 +141,16 @@ const Work = () => {
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                React JS Application
+              BlogBuster Web Application
               </span>
               <div className="pt-8 text-center">
-                <a href="/" target="_blank" rel="noopener noreferrer">
+                <a href="https://bloggbuster.netlify.app/" target="_blank" rel="noopener noreferrer">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
 
-                <a href="/" target="_blank" rel="noopener noreferrer">
+                <a href="https://github.com/Efo-Koku/blogbuster" target="_blank" rel="noopener noreferrer">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
